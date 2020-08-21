11dc624b82f1b23be952659c11bc7f409ab821fb
t.i <isoittech@gmail.com>
2020-08-21 13:41:45 +0900

    initial commit

---
+++ src/main/java/jp/isomurt/otameshi/heroku/firstherokusb/FirstHerokuSbApplication.java
@@ -0,0 +1,13 @@
+package jp.isomurt.otameshi.heroku.firstherokusb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstHerokuSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstHerokuSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/isomurt/otameshi/heroku/firstherokusb/FirstHerokuSbApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.isomurt.otameshi.heroku.firstherokusb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstHerokuSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
